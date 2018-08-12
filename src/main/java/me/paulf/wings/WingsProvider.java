363f37434fba9d645de2a2636edca1ed55f178c6
Paul Fulham <paul.fulham0@gmail.com>
2018-08-12 10:00:26 -0700

    Shrink wings provider structure

--- src/main/java/me/paulf/wings/WingsProvider.java
+++ src/main/java/me/paulf/wings/WingsProvider.java
@@ -1,20 +1,34 @@
 package me.paulf.wings;
 
-import it.unimi.dsi.fastutil.ints.IntList;
-import it.unimi.dsi.fastutil.ints.IntLists;
 import me.paulf.wings.server.integration.baubles.WingsBaubleHandler;
 import me.paulf.wings.util.ItemPlacing;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
-import net.minecraft.util.EnumFacing;
 import net.minecraftforge.fml.common.Loader;
-import net.minecraftforge.items.CapabilityItemHandler;
-import net.minecraftforge.items.IItemHandler;
 
 import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
 
 public final class WingsProvider {
-	private State state = new UninitializedState();
+	private State state = State.of(
+		s -> {
+			if (Loader.isModLoaded("baubles")) {
+				return State.of(UnaryOperator.identity(),
+					equip -> {
+						equip.accept(ItemPlacing.forArmor(EntityEquipmentSlot.CHEST));
+						equip.accept(WingsBaubleHandler.createBodyPlacing());
+					},
+					event -> event.accept(WingsBaubleHandler.createEventListener())
+				);
+			}
+			return State.of(UnaryOperator.identity(),
+				equip -> equip.accept(ItemPlacing.forArmor(EntityEquipmentSlot.CHEST)),
+				event -> {}
+			);
+		},
+		equip -> {},
+		event -> {}
+	);
 
 	public void addEventListeners(Consumer<Object> consumer) {
 		getState().addEventListeners(consumer);
@@ -34,71 +48,28 @@ public final class WingsProvider {
 		void addEquipmentPlacings(Consumer<ItemPlacing<EntityPlayer>> consumer);
 
 		void addEventListeners(Consumer<Object> consumer);
-	}
 
-	private static final class UninitializedState implements State {
-		private static final String BAUBLES_ID = "baubles";
-
-		@Override
-		public State get() {
-			final State basic = new BasicState();
-			return Loader.isModLoaded(BAUBLES_ID) ? new BaublesState(basic) : basic;
-		}
-
-		@Override
-		public void addEquipmentPlacings(Consumer<ItemPlacing<EntityPlayer>> consumer) {}
-
-		@Override
-		public void addEventListeners(Consumer<Object> consumer) {}
-	}
-
-	private static final class BasicState implements State {
-		@Override
-		public State get() {
-			return this;
-		}
-
-		@Override
-		public final void addEquipmentPlacings(Consumer<ItemPlacing<EntityPlayer>> consumer) {
-			consumer.accept(new ItemPlacing<EntityPlayer>() {
+		static State of(
+			UnaryOperator<State> next,
+			Consumer<Consumer<ItemPlacing<EntityPlayer>>> equipment,
+			Consumer<Consumer<Object>> events
+		) {
+			return new State() {
 				@Override
-				public IItemHandler getStorage(EntityPlayer player) {
-					return player.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, EnumFacing.NORTH);
+				public State get() {
+					return next.apply(this);
 				}
 
 				@Override
-				public IntList getSlots() {
-					return IntLists.singleton(EntityEquipmentSlot.CHEST.getIndex());
+				public void addEquipmentPlacings(Consumer<ItemPlacing<EntityPlayer>> consumer) {
+					equipment.accept(consumer);
 				}
-			});
-		}
 
-		@Override
-		public void addEventListeners(Consumer<Object> consumer) {}
-	}
-
-	private static final class BaublesState implements State {
-		private final State parent;
-
-		private BaublesState(State parent) {
-			this.parent = parent;
-		}
-
-		@Override
-		public State get() {
-			return this;
-		}
-
-		@Override
-		public void addEquipmentPlacings(Consumer<ItemPlacing<EntityPlayer>> consumer) {
-			this.parent.addEquipmentPlacings(consumer);
-			consumer.accept(WingsBaubleHandler.createBodyPlacing());
-		}
-
-		@Override
-		public void addEventListeners(Consumer<Object> consumer) {
-			consumer.accept(WingsBaubleHandler.createEventListener());
-			this.parent.addEventListeners(consumer);
+				@Override
+				public void addEventListeners(Consumer<Object> consumer) {
+					events.accept(consumer);
+				}
+			};
 		}
 	}
 }
--- src/main/java/me/paulf/wings/util/ItemPlacing.java
+++ src/main/java/me/paulf/wings/util/ItemPlacing.java
@@ -1,11 +1,30 @@
 package me.paulf.wings.util;
 
 import it.unimi.dsi.fastutil.ints.IntList;
+import it.unimi.dsi.fastutil.ints.IntLists;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.util.EnumFacing;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
 
 public interface ItemPlacing<T extends ICapabilityProvider> {
 	IItemHandler getStorage(T provider);
 
 	IntList getSlots();
+
+	static <T extends ICapabilityProvider> ItemPlacing<T> forArmor(EntityEquipmentSlot slot) {
+		return new ItemPlacing<T>() {
+			@Override
+			public IItemHandler getStorage(T provider) {
+				return provider.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, EnumFacing.NORTH);
+			}
+
+			@Override
+			public IntList getSlots() {
+				return IntLists.singleton(slot.getIndex());
+			}
+
+		};
+	}
 }
