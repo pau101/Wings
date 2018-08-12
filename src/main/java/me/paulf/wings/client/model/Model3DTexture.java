7712407997f3e20fcbf4aef9ba78021e66cf08c0
Paul Fulham <paul.fulham0@gmail.com>
2018-08-11 21:28:03 -0700

    Fix Model3DTexture winding

--- src/main/java/me/paulf/wings/client/model/Model3DTexture.java
+++ src/main/java/me/paulf/wings/client/model/Model3DTexture.java
@@ -46,47 +46,47 @@ public final class Model3DTexture extends ModelBox {
         float z0 = posZ1 * scale;
 		float z1 = (posZ1 + 1) * scale;
 		buf.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX_NORMAL);
-		buf.pos(x0, y0, z0).tex(u1, v1).normal(0.0F, 0.0F, -1.0F).endVertex();
 		buf.pos(x1, y0, z0).tex(u2, v1).normal(0.0F, 0.0F, -1.0F).endVertex();
-		buf.pos(x1, y1, z0).tex(u2, v2).normal(0.0F, 0.0F, -1.0F).endVertex();
+		buf.pos(x0, y0, z0).tex(u1, v1).normal(0.0F, 0.0F, -1.0F).endVertex();
 		buf.pos(x0, y1, z0).tex(u1, v2).normal(0.0F, 0.0F, -1.0F).endVertex();
-		buf.pos(x0, y1, z1).tex(u1, v2).normal(0.0F, 0.0F, 1.0F).endVertex();
+		buf.pos(x1, y1, z0).tex(u2, v2).normal(0.0F, 0.0F, -1.0F).endVertex();
 		buf.pos(x1, y1, z1).tex(u2, v2).normal(0.0F, 0.0F, 1.0F).endVertex();
-		buf.pos(x1, y0, z1).tex(u2, v1).normal(0.0F, 0.0F, 1.0F).endVertex();
+		buf.pos(x0, y1, z1).tex(u1, v2).normal(0.0F, 0.0F, 1.0F).endVertex();
 		buf.pos(x0, y0, z1).tex(u1, v1).normal(0.0F, 0.0F, 1.0F).endVertex();
+		buf.pos(x1, y0, z1).tex(u2, v1).normal(0.0F, 0.0F, 1.0F).endVertex();
 		float f5 = 0.5F * (u1 - u2) / width;
 		float f6 = 0.5F * (v1 - v2) / height;
 		for (int k = 0; k < width; k++) {
 			float f7 = x0 + k * scale;
 			float f8 = u1 + (u2 - u1) * ((float) k / width) - f5;
-			buf.pos(f7, y0, z1).tex(f8, v1).normal(-1.0F, 0.0F, 0.0F).endVertex();
 			buf.pos(f7, y0, z0).tex(f8, v1).normal(-1.0F, 0.0F, 0.0F).endVertex();
-			buf.pos(f7, y1, z0).tex(f8, v2).normal(-1.0F, 0.0F, 0.0F).endVertex();
+			buf.pos(f7, y0, z1).tex(f8, v1).normal(-1.0F, 0.0F, 0.0F).endVertex();
 			buf.pos(f7, y1, z1).tex(f8, v2).normal(-1.0F, 0.0F, 0.0F).endVertex();
+			buf.pos(f7, y1, z0).tex(f8, v2).normal(-1.0F, 0.0F, 0.0F).endVertex();
 		}
 		for (int k = 0; k < width; k++) {
 			float f8 = u1 + (u2 - u1) * ((float) k / width) - f5;
 			float f9 = x0 + (k + 1) * scale;
-			buf.pos(f9, y1, z1).tex(f8, v2).normal(1.0F, 0.0F, 0.0F).endVertex();
 			buf.pos(f9, y1, z0).tex(f8, v2).normal(1.0F, 0.0F, 0.0F).endVertex();
-			buf.pos(f9, y0, z0).tex(f8, v1).normal(1.0F, 0.0F, 0.0F).endVertex();
+			buf.pos(f9, y1, z1).tex(f8, v2).normal(1.0F, 0.0F, 0.0F).endVertex();
 			buf.pos(f9, y0, z1).tex(f8, v1).normal(1.0F, 0.0F, 0.0F).endVertex();
+			buf.pos(f9, y0, z0).tex(f8, v1).normal(1.0F, 0.0F, 0.0F).endVertex();
 		}
 		for (int k = 0; k < height; k++) {
 			float f8 = v1 + (v2 - v1) * ((float) k / height) - f6;
 			float f9 = y0 + (k + 1) * scale;
-			buf.pos(x0, f9, z0).tex(u1, f8).normal(0.0F, 1.0F, 0.0F).endVertex();
 			buf.pos(x1, f9, z0).tex(u2, f8).normal(0.0F, 1.0F, 0.0F).endVertex();
-			buf.pos(x1, f9, z1).tex(u2, f8).normal(0.0F, 1.0F, 0.0F).endVertex();
+			buf.pos(x0, f9, z0).tex(u1, f8).normal(0.0F, 1.0F, 0.0F).endVertex();
 			buf.pos(x0, f9, z1).tex(u1, f8).normal(0.0F, 1.0F, 0.0F).endVertex();
+			buf.pos(x1, f9, z1).tex(u2, f8).normal(0.0F, 1.0F, 0.0F).endVertex();
 		}
 		for (int k = 0; k < height; k++) {
 			float f7 = y0 + k * scale;
 			float f8 = v1 + (v2 - v1) * ((float) k / height) - f6;
-			buf.pos(x1, f7, z0).tex(u2, f8).normal(0.0F, -1.0F, 0.0F).endVertex();
 			buf.pos(x0, f7, z0).tex(u1, f8).normal(0.0F, -1.0F, 0.0F).endVertex();
-			buf.pos(x0, f7, z1).tex(u1, f8).normal(0.0F, -1.0F, 0.0F).endVertex();
+			buf.pos(x1, f7, z0).tex(u2, f8).normal(0.0F, -1.0F, 0.0F).endVertex();
 			buf.pos(x1, f7, z1).tex(u2, f8).normal(0.0F, -1.0F, 0.0F).endVertex();
+			buf.pos(x0, f7, z1).tex(u1, f8).normal(0.0F, -1.0F, 0.0F).endVertex();
 		}
 		tessellator.draw();
 	}
