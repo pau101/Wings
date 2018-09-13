2fb697075ac76ab963642e66734b7ee63dbe26a0
Paul Fulham <paul.fulham0@gmail.com>
2018-09-12 17:28:59 -0700

    Release 1.0.6

--- src/main/java/me/paulf/wings/WingsMod.java
+++ src/main/java/me/paulf/wings/WingsMod.java
@@ -22,7 +22,7 @@ public final class WingsMod {
 
 	public static final String NAME = "Wings";
 
-	public static final String VERSION = "1.0.5";
+	public static final String VERSION = "1.0.6";
 
 	private static final class Holder {
 		private static final WingsMod INSTANCE = new WingsMod();
