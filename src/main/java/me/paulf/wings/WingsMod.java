2f14cc46806daa0a8cb6b2a000474384a6a47e46
Paul Fulham <paul.fulham0@gmail.com>
2018-10-06 13:45:35 -0700

    Improve build script

--- src/main/java/me/paulf/wings/WingsMod.java
+++ src/main/java/me/paulf/wings/WingsMod.java
@@ -11,18 +11,9 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 import java.util.function.Consumer;
 
-@Mod(
-	modid = WingsMod.ID,
-	name = WingsMod.NAME,
-	version = WingsMod.VERSION,
-	dependencies = "required-after:forge@[14.23.4.2705,);required-after:llibrary@[1.7,1.8)"
-)
+@Mod(modid = WingsMod.ID, useMetadata = true)
 public final class WingsMod {
 	public static final String ID = "wings";
-
-	public static final String NAME = "Wings";
-
-	public static final String VERSION = "1.0.6";
 
 	private static final class Holder {
 		private static final WingsMod INSTANCE = new WingsMod();
