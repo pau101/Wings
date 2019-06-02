3982e5e19ffbcecc89f97b3067ce943fc902b72c
Kyle Koder <silvertallon@gmail.com>
2019-06-02 14:06:32 -0700

    Changed config category from "" to "general" (the default) to prevent forge startup crash (not allowed to put non-categories into a blank category)

--- src/main/java/me/paulf/wings/server/config/WingsConfig.java
+++ src/main/java/me/paulf/wings/server/config/WingsConfig.java
@@ -3,7 +3,7 @@ package me.paulf.wings.server.config;
 import me.paulf.wings.WingsMod;
 import net.minecraftforge.common.config.Config;
 
-@Config(modid = WingsMod.ID, name = WingsMod.ID + "/wings", category = "")
+@Config(modid = WingsMod.ID, name = WingsMod.ID + "/wings")
 public final class WingsConfig
 {
 	@Config.LangKey("config.wings.armorblacklist")
