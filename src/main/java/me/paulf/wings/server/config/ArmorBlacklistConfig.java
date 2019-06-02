7914930f4176b9991906060d0f9735565319ef5a
Laike-Endaril <silvertallon@gmail.com>
2019-06-02 13:58:33 -0700

    Change category from "armorblacklist/armorblacklist" to "wings/"
    Co-Authored-By: Paul Fulham <paul.fulham0@gmail.com>

--- src/main/java/me/paulf/wings/server/config/ArmorBlacklistConfig.java
+++ src/main/java/me/paulf/wings/server/config/ArmorBlacklistConfig.java
@@ -3,7 +3,7 @@ package me.paulf.wings.server.config;
 import me.paulf.wings.WingsMod;
 import net.minecraftforge.common.config.Config;
 
-@Config(modid = WingsMod.ID, name = WingsMod.ID + "/armorblacklist", category = "armorblacklist")
+@Config(modid = WingsMod.ID, name = WingsMod.ID + "/wings", category = "")
 public final class ArmorBlacklistConfig {
 	@Config.LangKey("config.wings.armorblacklist")
 	public static String[] disallowedItems = new String[] {
